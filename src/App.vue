fce51b2fc8f54975375a90d7951ebef7c3985722
lihumm <511650406@qq.com>
2019-02-01 03:43:18 +0800

    首页获取数据完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
-    <FooterGuide v-show="$route.meta.showFooter" />
+    <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
 <script>
 import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
 export default {
+  async mounted() {
+    const result = await reqFoodTypes()
+    console.log(result)
+  },
   components: {
     FooterGuide
   }
@@ -15,9 +20,10 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheep/stylus">
-  .app 
-    width 100%
-    height 100%
-    background #f5f5f5
+.app {
+  width: 100%;
+  height: 100%;
+  background: #f5f5f5;
+}
 </style>
 
--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,14 @@
  * 函数的返回值:promise
  */
 import ajax from './ajax'
+//const BASE_URL = 'http://localhost:4000'
+const BASE_URL = '/api'
 // [1、根据经纬度获取位置详情](#1根据经纬度获取位置详情)<br/>
-export const reqAddress = (geohash) => ajax('/position/${geohash}')
+export const reqAddress = (geohash) => ajax('${BASE_URL}/position/${geohash}')
 // [2、获取食品分类列表](#2获取食品分类列表)<br/>
-export const reqFoodTypes = () => ajax('/index_category')
+export const reqFoodTypes = () => ajax(BASE_URL + '/index_category')
 // [3、根据经纬度获取商铺列表](#3根据经纬度获取商铺列表)<br/>
-export const reqShops = (latitude, longitude) => ajax('/shops', {
+export const reqShops = (latitude, longitude) => ajax(BASE_URL + '/shops', {
   latitude,
   longitude
 })
